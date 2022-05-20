4475696d4529ec4cc8ccaca7d7472cea91978125
Dominik Jędrzejowski <dominik.jedrzejowski@apator.com>
2022-05-20 14:40:41 +0200

    Enable Virtual Terminal Processing

--- console-app/src/main/java/org/scrum/psd/battleship/ascii/Main.java
+++ console-app/src/main/java/org/scrum/psd/battleship/ascii/Main.java
@@ -6,6 +6,9 @@ import org.scrum.psd.battleship.controller.GameController;
 import org.scrum.psd.battleship.controller.dto.Letter;
 import org.scrum.psd.battleship.controller.dto.Position;
 import org.scrum.psd.battleship.controller.dto.Ship;
+import com.sun.jna.*;
+import com.sun.jna.platform.win32.WinDef.*;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
 
 import java.util.List;
 import java.util.Random;
@@ -19,6 +22,25 @@ public class Main {
     private static List<Ship> enemyFleet;
 
     public static void main(String[] args) {
+
+        if(System.getProperty("os.name").startsWith("Windows"))
+        {
+            // Set output mode to handle virtual terminal sequences
+            Function GetStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");
+            DWORD STD_OUTPUT_HANDLE = new DWORD(-11);
+            HANDLE hOut = (HANDLE)GetStdHandleFunc.invoke(HANDLE.class, new Object[]{STD_OUTPUT_HANDLE});
+
+            DWORDByReference p_dwMode = new DWORDByReference(new DWORD(0));
+            Function GetConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");
+            GetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, p_dwMode});
+
+            int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;
+            DWORD dwMode = p_dwMode.getValue();
+            dwMode.setValue(dwMode.intValue() | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+            Function SetConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");
+            SetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, dwMode});
+        }
+
         System.out.println(colorize("                                     |__", MAGENTA_TEXT()));
         System.out.println(colorize("                                     |\\/", MAGENTA_TEXT()));
         System.out.println(colorize("                                     ---", MAGENTA_TEXT()));
